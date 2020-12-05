9aab78a0f1757e941e7b5f8d1effa27341721452
xJuhix <49311688+xJuhix@users.noreply.github.com>
2020-12-05 20:22:16 +0100

    trying to fix search and some styling

--- src/components/SearchRecipe.js
+++ src/components/SearchRecipe.js
@@ -1,13 +1,20 @@
 import React from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import PropTypes from "prop-types";
 
 
 
 function RecipeSearch({ handleSearch }) {
 	return (
-		<input className="search" placeholder="Search by name..." onChange={event => handleSearch(event)} />
-		
+			<InputGroup className="search">
+				<FormControl placeholder="Search by name..." onChange={event => handleSearch(event)} />
+			</InputGroup>
 	);
 }
 
+RecipeSearch.propTypes = {
+	handleSearch: PropTypes.func.isRequired
+};
 
 export default RecipeSearch;
